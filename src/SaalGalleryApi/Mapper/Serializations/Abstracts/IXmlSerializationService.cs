679860dd3539f11fbb28bc13880eda43c1f9c6d4
Elvin Reyes <elvinjreyesv@outlook.com>
2025-01-29 08:32:41 +0000

    remove unused map

--- src/SaalGalleryApi/Mapper/Serializations/Abstracts/IXmlSerializationService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SaalGallery.Mapper.Serializations.Abstracts;
-
-public interface IXmlSerializationService
-{
-    T Deserialize<T>(string input) where T : class;
-    T DeserializeWithEmptySchema<T>(string input) where T : class;
-
-    string Serialize<T>(T input);
-}
